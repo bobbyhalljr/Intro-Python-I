db222baa1747a316df967ee94b4fcf0b1649d727
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-14 19:16:01 -0500

    have 3 more left

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,13 +11,15 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+    
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for var in range(0, 10):
+    y.append(var ** 3)
 
 print(y)
 
@@ -27,6 +29,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for i in a:
+    y.append(i.upper())
 
 print(y)
 
@@ -36,6 +40,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-
+y = [i for i in x if int(i) % 2 == 0]
+    
 print(y)
--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,7 +4,7 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
-def change_x():
+def change_x(x):
     x = 99
 
 change_x()
